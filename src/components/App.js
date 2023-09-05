7bf5e5a7e693d7fab9e87c0fc4b3ac77bc451630
SerhiiRud <carmagsales@gmail.com>
2023-09-05 22:35:14 +0300

    structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Loader } from "./Loader/Loader";
 
-function App() {
-  return <div>App</div>;
-}
+const Home = lazy(() => import("pages/Home/Home"));
+const Sidebar = lazy(() => import("components/Sidebar/Sidebar"));
+const Catalog = lazy(() => import("pages/Catalog/Catalog"));
+const Favorites = lazy(() => import("pages/Favorites/Favorites"));
 
-export default App;
+export const App = () => {
+  <Suspense fallback={<Loader />}>
+    <Routes>
+      <Route path="/" element={<Sidebar />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </Suspense>;
+};
---
+++ src/components/Filterbar/Filterbar.jsx
@@ -0,0 +1,5 @@
+import { Outlet } from "react-router-dom";
+
+const Filterbar = () => {
+  return <div>Filterbar</div>;
+};
