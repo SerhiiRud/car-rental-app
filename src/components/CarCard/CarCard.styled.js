791d02d2acbebeb4b8ee0cce1671d5410473f793
SerhiiRud <carmagsales@gmail.com>
2023-09-09 22:15:24 +0300

    Updates

--- src/components/CarCard/CarCard.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "@emotion/styled";
-
-export const FavoriteButton = styled.button`
-  background-color: ${({ isFavorite }) => (isFavorite ? "green" : "red")};
-`;
--- src/components/Filterbar/Filterbar.jsx
+++
@@ -1,7 +0,0 @@
-//import { Outlet } from "react-router-dom";
-
-const Filterbar = () => {
-  return <div>Filterbar</div>;
-};
-
-export default Filterbar;
